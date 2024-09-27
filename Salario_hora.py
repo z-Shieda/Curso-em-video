64e745ed8056d02c0a52bc19cdb63fcc0b89226c
z-Shieda <134968579+z-Shieda@users.noreply.github.com>
2024-09-27 15:03:18 -0300

    Delete Salario_hora.py

--- Salario_hora.py
+++
@@ -1,7 +0,0 @@
-print("Calculadora de salário")
-valores_h = float(input("Digite o valor do seu salário em hora:"))
-horas_t = float(input("Quantas horas foram trabalhadas no mês:"))
-
-calc = valores_h * horas_t
-
-print("O seu rendimento mensal foi de R${}".format(calc))
